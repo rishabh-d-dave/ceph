538c430fc1eca100be1fe12c63da668ee9ea1321
Rishabh Dave <ridave@redhat.com>
2022-05-31 08:44:51 +0530

    qa/cephfs: save results dir from xfstests-dev repo
    The "results" directory in xfstests-dev repo contains logs that are
    created by it during execution of its tests. Copying this directory to
    Ceph log directory saves it from being erased during tear down conducted
    by teuthology test runner. These logs allow deeper inspection in to the
    cause of failure of tests.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/cephfs/xfstests_dev.py
+++ qa/tasks/cephfs/xfstests_dev.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from os.path import join
 from logging import getLogger
 
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
@@ -13,6 +14,8 @@ log = getLogger(__name__)
 # CephFS before running tests using kernel, so ceph-fuse mounts are never
 # actually testsed.
 class XFSTestsDev(CephFSTestCase):
+
+    RESULTS_DIR = "results"
 
     def setUp(self):
         super(XFSTestsDev, self).setUp()
@@ -52,12 +55,48 @@ class XFSTestsDev(CephFSTestCase):
                                        omit_sudo=False, check_status=False)
 
     def del_repos(self):
+        self.save_results_dir()
         self.mount_a.client_remote.run(args=f'sudo rm -rf {self.xfstests_repo_path}',
                                        omit_sudo=False, check_status=False)
 
         if self.install_xfsprogs:
             self.mount_a.client_remote.run(args=f'sudo rm -rf {self.xfsprogs_repo_path}',
                                            omit_sudo=False, check_status=False)
+
+    def save_results_dir(self):
+        """
+        When tests in xfstests-dev repo are executed, logs are created and
+        saved, under a directory named "results" that lies at the repo root.
+        In case a test from xfstests-dev repo fails, these logs will help find
+        the cause of the failure.
+
+        Since there's no option in teuthology to copy a directory lying at a
+        custom location in order to save it from teuthology test runner's tear
+        down, let's copy this directory to a standard location that teuthology
+        copies away before erasing all data on the test machine. The standard
+        location chosen in the case here is the Ceph log directory.
+
+        In case of vstart_runner.py, this methods does nothing.
+        """
+        # No need to save results dir in case of vstart_runner.py.
+        for x in ('LocalFuseMount', 'LocalKernelMount'):
+            if x in self.mount_a.__class__.__name__:
+                return
+
+        src = join(self.xfstests_repo_path, self.RESULTS_DIR)
+
+        if self.mount_a.run_shell(f'sudo stat {src}',
+                check_status=False, omit_sudo=False).returncode != 0:
+            log.info(f'xfstests-dev repo contains not directory named '
+                     f'"{self.RESULTS_DIR}". repo location: {self.xfstests_repo_path}')
+            return
+
+        std_loc = '/var/log/ceph' # standard location
+        dst = join(std_loc, 'xfstests-dev-results')
+        self.mount_a.run_shell(f'sudo mkdir -p {dst}', omit_sudo=False)
+        self.mount_a.run_shell(f'sudo cp -r {src} {dst}', omit_sudo=False)
+        log.info(f'results dir from xfstests-dev has been saved; it was '
+                 f'copied from {self.xfstests_repo_path} to {std_loc}.')
 
     def build_and_install(self):
         # NOTE: On teuthology machines it's necessary to run "make" as
@@ -112,8 +151,6 @@ class XFSTestsDev(CephFSTestCase):
             and "scratch" directories would be mounted. Look at xfstests-dev
             local.config's template inside this file to get some context.
         """
-        from os.path import join
-
         self.test_dirname = 'test'
         self.mount_a.run_shell(['mkdir', self.test_dirname])
         # read var name as "test dir's mount path"
@@ -187,7 +224,6 @@ class XFSTestsDev(CephFSTestCase):
                                        check_status=False)
 
     def write_local_config(self):
-        from os.path import join
         from textwrap import dedent
 
         mon_sock = self.fs.mon_manager.get_msgrv1_mon_socks()[0]
