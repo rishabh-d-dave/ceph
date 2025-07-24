54d46dc9d285fbd106963c5fb33d4762ad2b4cac
Rishabh Dave <ridave@redhat.com>
2025-07-24 17:24:40 +0530

    DNM: run only one test
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ex
 
+
 # Running as root because the filesystem root directory will be
 # owned by uid 0, and that's where we're writing.
-sudo python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
+sudo python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py -k test_rmtree_no_perm_on_nonroot_dir_supress_errors
 exit 0
