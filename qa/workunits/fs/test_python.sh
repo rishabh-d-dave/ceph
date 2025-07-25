e75b596957f04af625c78d48867043d2851adced
Rishabh Dave <ridave@redhat.com>
2025-07-25 13:50:06 +0530

    DNM: temp
    prinitng UID, GID and permission on root of cephfs
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -ex
 
+username=$(id -un)
+sudo chown -R $username:$username $(dirname $0)/../../../../
+sudo chown -R $username:$username ./
 
-# Running as root because the filesystem root directory will be
-# owned by uid 0, and that's where we're writing.
-sudo python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py -k test_rmtree_no_perm_on_nonroot_dir_supress_errors
+python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
+
+# restoring original owner.
+sudo chown -R root:root $(dirname $0)/../../../../
+sudo chown -R root:root ./
 exit 0
