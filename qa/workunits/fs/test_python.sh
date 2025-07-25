b3a9457234c7365c06fa5fcfa86d3548c6786cb2
Rishabh Dave <ridave@redhat.com>
2025-07-25 13:50:06 +0530

    qa, test: run unit tests for cephfs.pyx with non-root user
    In test_python.sh -
    - change owner of files to non-root user's UID and GID
    - run test_cephfs.py without sudo
    
    In test_cephfs.py -
    - avoid mounting with root, set client_mount_uid and client_mount_gid to
      process's UID and GID
    - change owner of file system's root to process's UID and GID.
    
    Running test_cephfs.py as root (and therefore keeping file system's user
    as root) is being avoided for certains tests that ensure that the file
    permissions are respected to allow/disllow certain FS operations.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -ex
 
-# Running as root because the filesystem root directory will be
-# owned by uid 0, and that's where we're writing.
-sudo python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
+username=$(id -un)
+sudo chown -R $username:$username $(dirname $0)/../../../../
+sudo chown -R $username:$username ./
+
+python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
+
+# restoring original owner.
+sudo chown -R root:root $(dirname $0)/../../../../
+sudo chown -R root:root ./
 exit 0
