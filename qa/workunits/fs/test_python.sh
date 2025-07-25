f45b9260f014b02e8f93d77fea56b2974d651ac3
Rishabh Dave <ridave@redhat.com>
2025-07-25 13:50:06 +0530

    qa, test: run unit tests for cephfs.pyx with non-root user
    Run test_python.sh with non-root user. This makes it necessary to change
    the owner user and group of file system root to be same as this non-root
    user. This brings testing closer to the real-world scenario and also
    allows exercising negative tests where an FS op would fail for a non-root
    user but it would pass for root user.
    
    There are few tests that exercise FS operations where root user is
    needed. Group these tests under a separate class and add extra code for
    this class that allows these tests to run with root UID and GID.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
-# Running as root because the filesystem root directory will be
-# owned by uid 0, and that's where we're writing.
-sudo python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
+username=$(id -un)
+sudo chown -R $username:$username $(dirname $0)/../../../../
+
+python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
+
 exit 0
