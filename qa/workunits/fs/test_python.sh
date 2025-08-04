e761982e0ca660867aedab3547fa6b8b76563219
Rishabh Dave <ridave@redhat.com>
2025-08-04 16:12:49 +0530

    DNM: print permission and owners of some directories
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -1,12 +1,5 @@
 #!/bin/sh -ex
 
-username=$(id -un)
-sudo chown -R $username:$username $(dirname $0)/../../../../
-sudo chown -R $username:$username ./
-
 python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py
 
-# restoring original owner.
-sudo chown -R root:root $(dirname $0)/../../../../
-sudo chown -R root:root ./
 exit 0
