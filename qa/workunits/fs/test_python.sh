6e01ba80bb7b8ed075e54f916e74e3b11442eab8
Rishabh Dave <ridave@redhat.com>
2025-10-31 12:20:57 +0530

    qa/cephfs: print perf dump at the end of test
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -3,4 +3,10 @@
 # Running as root because the filesystem root directory will be
 # owned by uid 0, and that's where we're writing.
 python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py -k test_create_and_rm_2000_subdir_levels_close_v4
+
+set +e
+ceph tell mds.0 perf dump
+ceph tell mds.0 perf dump
+set -e
+
 exit 0
