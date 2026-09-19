756d117ee7f1de0510b5cf37f25b9dbda95c17a5
Rishabh Dave <ridave@redhat.com>
2026-09-19 16:07:42 +0530

    qa/cephfs: add a redundant line to indicate type to linters
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/cephfs/cephfs_test_case.py
+++ qa/tasks/cephfs/cephfs_test_case.py
@@ -121,6 +121,10 @@ class CephFSTestCase(CephTestCase):
     def setUp(self):
         super(CephFSTestCase, self).setUp()
 
+        assert type(self.mounts) is list and len(self.mounts) > 0
+        # indicate types to linters
+        self.mounts = setattr(self, 'mounts', [])
+
         self.config_set('mon', 'mon_allow_pool_delete', True)
 
         if len(self.mds_cluster.mds_ids) < self.MDSS_REQUIRED:
