0d355eeec781119d43da6b134950f8954cdd45ef
Rishabh Dave <ridave@redhat.com>
2026-09-24 17:10:01 +0530

    qa/cephfs: refactor loop for readability in update_attrs()
    New manner is more readable as well as a standard Pythonic phrase.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/cephfs/mount.py
+++ qa/tasks/cephfs/mount.py
@@ -574,8 +574,7 @@ class CephFSMountBase(object):
 
         self._verify_attrs(**{key: kwargs[key] for key in verify_keys if key in kwargs})
 
-        for k in verify_keys:
-            v = kwargs.get(k)
+        for k, v in verify_keys.items():
             setattr(self, k, v)
 
     def remount(self, **kwargs):
