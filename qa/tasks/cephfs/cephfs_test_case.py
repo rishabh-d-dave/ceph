882dd63b60436080d1dfaafec76101294357731f
Rishabh Dave <ridave@redhat.com>
2026-09-24 17:10:04 +0530

    mgr: fix pylint errors in .pylintrc
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/cephfs/cephfs_test_case.py
+++ qa/tasks/cephfs/cephfs_test_case.py
@@ -122,8 +122,8 @@ class CephFSTestCase(CephTestCase):
         super(CephFSTestCase, self).setUp()
 
         assert type(self.mounts) is list and len(self.mounts) > 0
-        # indicate types to linters
-        self.mounts = setattr(self, 'mounts', [])
+        # dummy statement to indicate type to prevent unnecessary linter warning
+        self.mounts = self.mounts if self.mounts else []
 
         self.config_set('mon', 'mon_allow_pool_delete', True)
 
